41b6e9d9a5d160a9e9ff33d224b33221e4746678
Smint <smint-russia@mail.ru>
2023-01-29 16:28:26 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Это программа, которая на вход принимает любые значения/символы/буквы,
+а на выходе показыват только те строки, где число символов меньше или равно 3.
+*/
+
+Console.WriteLine("Введите, что хотите через ПРОБЕЛ");
+
+string symbol = Convert.ToString(Console.ReadLine());
+string[] text = symbol.Split(' ');
+
+    for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i].Length > 3)
+                {
+                    text[i] = "";
+                }
+        Console.Write(text[i] + " ");
+        }
